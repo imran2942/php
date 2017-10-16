f4fe8cb5a41541b1b7f189eb8920e2ff133136f6
imran <imran@prime.com>
2017-10-16 18:56:22 +0500

    first c

---
+++ loop.php
@@ -0,0 +1,18 @@
+<?php
+
+ echo "this is first php program";
+
+ $w=1;
+ while($w>10){
+
+ 	echo $w;
+ 	w+=1;
+
+ } 
+ for ($i=0; $i <10 ; $i++) {
+
+    echo "this is for loop number".$i; 
+ 	# code...
+ }
+
+ ?>
